4556a61e4fb8c462391a9a31788ed5020cfdbb63
Jiwoo Hwang <jiwoohwang@macbook-pro.myfiosgateway.com>
2022-07-07 11:44:40 -0400

    data pulled using new search.py

--- sample_data/search.py
+++ sample_data/search.py
@@ -2,14 +2,14 @@
 
 # This sample executes a search request for the specified search term.
 # Sample usage:
-#   python3 search.py --q=surfing --max-results=20
+#   python search.py --q=surfing --max-results=20
 # NOTE: To use the sample, you must provide a developer key obtained
 #       in the Google APIs Console. Search for "REPLACE_ME" in this code
 #       to find the correct place to provide that key..
 
 import argparse
 import json
-import numpy as np
+import pandas as pd
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -19,14 +19,14 @@ from googleapiclient.errors import HttpError
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = 'AIzaSyDAptpTfh33KDwIuyVDB714gVVBe9yYIwE'
+
+DEVELOPER_KEY = 'AIzaSyC_Z_VCyF6j8nWrVEtgG2WmBW3tHOwyx9A'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
 def youtube_search(options):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
-
     # Call the search.list method to retrieve results matching the specified
     # query term.
     search_response = youtube.search().list(
@@ -35,75 +35,31 @@ def youtube_search(options):
         maxResults=options.max_results
     ).execute()
 
-    videos = []
-    stat_names = ['title', 'videoId', 'channelId', 'channelTitle', 'viewCount', 'likeCount', 'favorite count', 'commentCount']
-    # Add each result to the appropriate list, and then display the lists of
-    # matching videos, channels, and playlists.
-    
+    videos = {}
+    i=0
+
     for search_result in search_response.get('items', []):
+
         if search_result['id']['kind'] == 'youtube#video':
-            videos.append(search_result['snippet']['title'])
-            videos.append(search_result['id']['videoId'])
-            videos.append(search_result['snippet']['channelId'])
-            videos.append(search_result['snippet']['channelTitle'])
+            
             search_response3 = youtube.videos().list(
                 id=search_result['id']['videoId'],
-                part='statistics'
+                part='statistics,contentDetails,liveStreamingDetails,localizations,snippet,player,recordingDetails,snippet,status,topicDetails'
             ).execute()
-            for search_result in search_response3.get('items',[]):
-                try:
-                    videos.append(search_result['statistics']['viewCount'])
-                    break
-                except:
-                    videos.append("no data")
-            for search_result in search_response3.get('items',[]):    
-                try:
-                    videos.append(search_result['statistics']['likeCount'])
-                    break
-                except:
-                    videos.append("no data")
-            for search_result in search_response3.get('items',[]):
-                try:
-                    videos.append(search_result['statistics']['favoriteCount'])
-                    break
-                except:
-                    videos.append("no data")
-            for search_result in search_response3.get('items',[]):
-                try:
-                    videos.append(search_result['statistics']['commentCount'])
-                    break
-                except:
-                    videos.append("no data")
 
-    for i in range(int((len(videos)/8))-1):
-        stat_names.append('title')
-        stat_names.append('videoId')
-        stat_names.append('channelId')
-        stat_names.append('channelTitle')
-        stat_names.append('viewCount')
-        stat_names.append('likeCount')
-        stat_names.append('favoriteCount')
-        stat_names.append('commentCount')
-    
-    stat_names = np.array_split(stat_names, int((len(videos)/8)))
-    videos = np.array_split(videos, int((len(videos)/8)))
-    res = {}
-    res2 = {}
-    for i in range(len(stat_names)):
-        arr = stat_names[i]
-        arr2 = videos[i]
-        res2 = dict(zip(arr,arr2))
-        res[i] = res2
-        
-    
-    #res = dict(zip(stat_names, videos))
-    print ("stat names length: " + str(len(stat_names)))
+            videos[i] = search_response3.get('items',[])
+        else: 
+            videos[i] = search_result
+
+        i = i+1
+
+    res = videos
     print ("videos length: " + str(len(videos)))
-    #print ("Resultant dictionary is : " +  str(res))
     
     
-    file_name = 'game_evilNun_videos.json'
+    file_name = 'game_phasmophobia.json'
     import os
+
     # check if size of file is 0
     with open(file_name, 'w') as f:
        json_object = json.dumps(res, indent = 4)
@@ -111,10 +67,10 @@ def youtube_search(options):
        json.dump(z, f, indent = 4)
     
     print('file dumped')
-
-    
     
 
+    data = pd.read_json("game_phasmophobia.json")
+    pd.json_normalize(data.values())
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
