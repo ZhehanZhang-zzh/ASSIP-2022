3a04d3afe2c9a1c144234d3a7ec314298a4d2343
kaavyar9705 <kaavyakri@gmail.com>
2022-07-07 15:46:38 -0400

    tocsv

--- sample_data/toCSV.py
+++ sample_data/toCSV.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 project_file = "beauty_fenty.csv"
 
-with open('beauty_fenty.json') as project_file:    
+with open('beauty_fenty.json', 'r+', encoding="utf-8") as project_file:    
     data = json.load(project_file)  
 
-df = pd.json_normalize(data)
-df.to_csv(project_file, encoding='utf-8')
+    df = pd.json_normalize(data)
+    df.to_csv(project_file, encoding='utf-8', index=False)
