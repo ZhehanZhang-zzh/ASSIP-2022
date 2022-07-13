88c1ccf98cd5512d3290dff280747fdf640e23af
Scarlett Adams <scarlett.j.adams@gmail.com>
2022-07-13 14:22:51 -0400

    search.py pagination

--- sample_data/search.py
+++ sample_data/search.py
@@ -2,7 +2,7 @@
 
 # This sample executes a search request for the specified search term.
 # Sample usage:
-#   python search.py --q=makeup --max-results=50
+#   python search.py --q=makeup --max-results=100
 # NOTE: To use the sample, you must provide a developer key obtained
 #       in the Google APIs Console. Search for "REPLACE_ME" in this code
 #       to find the correct place to provide that key..
@@ -15,13 +15,12 @@ import pprint
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
 
-DEVELOPER_KEY = 'AIzaSyDAptpTfh33KDwIuyVDB714gVVBe9yYIwE'
+DEVELOPER_KEY = 'AIzaSyA1qbVvM7G6xRg-pC7o4feZDrhfgxyH84c'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
@@ -37,49 +36,62 @@ def youtube_search(options):
         maxResults=options.max_results
     ).execute()
 
-    videos = {}
-    channels = {}
     # stat_names = ['title', 'videoId', 'channelId', 'channelTitle', 'viewCount', 'likeCount', 'favorite count', 'commentCount']
     # Add each result to the appropriate list, and then display the lists of
     # matching videos, channels, and playlists.
     i=0
 
-    for search_result in search_response.get('items'):
+    nextPageToken = search_response['nextPageToken']
+    videos = {}
 
-        if search_result['id']['kind'] == 'youtube#video':
+    while (nextPageToken):
+
+        if i%50==0 and i!=0:
             
-            search_response3 = youtube.videos().list(
-                id=search_result['id']['videoId'],
-                part='statistics,contentDetails,liveStreamingDetails,localizations,snippet,player,recordingDetails,snippet,status,topicDetails'
+            print("In the loop:" + str(nextPageToken))
+
+            search_response = youtube.search().list(
+                q=options.q,
+                part='id,snippet',
+                maxResults=options.max_results,
+                pageToken=nextPageToken
             ).execute()
 
-            videos[i] = search_response3.get('items',[])[0]
-        else: 
-            channels[i] = search_result
+            nextPageToken = search_response['nextPageToken']
+            videos = {}
 
-        i = i+1
+        for search_result in search_response['items']:
+            if search_result['id']['kind'] == 'youtube#video':
+                
+                search_response3 = youtube.videos().list(
+                    id=search_result['id']['videoId'],
+                    part='statistics,contentDetails,liveStreamingDetails,localizations,snippet,player,recordingDetails,snippet,status,topicDetails'
+                ).execute()       
 
-    res = videos
-    # print ("stat names length: " + str(len(stat_names)))
-    print ("videos length: " + str(len(videos)))
-    #print ("Resultant dictionary is : " +  str(res))
-    
-    
+                # videos[i] = search_response3
 
-    file_name_videos = 'beauty_makeupCollab.json'
-    file_name_channels = 'beauty_channels.json'
+                videos[i] = search_response3.get('items',[])[0]
+            # else: 
+            #     videos[i] = search_result[0]
 
-    import os
-    
-    # check if size of file is 0
-    with open(file_name_videos, 'w') as f:
-       json_object = json.dumps(res, indent = 4)
-       z = json.loads(json_object)
-       json.dump(z, f, indent = 4)
-    
-    
+            i = i+1
+
+        res = videos
+        # print ("stat names length: " + str(len(stat_names)))
+        print ("videos length: " + str(len(videos)))
+        #print ("Resultant dictionary is : " +  str(res))
         
-    print('file dumped')
+        file_name = 'videos' + str(i) + '.json'
+        import os
+        file_path = 'videos' + str(i) + '.json'
+
+        # check if size of file is 0
+        with open(file_name, 'w') as f:
+           json_object = json.dumps(res, indent = 4)
+           z = json.loads(json_object)
+           json.dump(z, f, indent = 4)
+        
+        print('file dumped')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
