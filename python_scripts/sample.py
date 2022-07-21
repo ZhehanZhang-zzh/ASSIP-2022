98fc67c94c18a35309ebb496da396db47aa058a7
Arnav <arnav.mach@gmail.com>
2022-07-21 10:14:16 -0500

    Updated to include new data and updated sample.py accordingly

--- python_scripts/sample.py
+++ python_scripts/sample.py
@@ -5,7 +5,7 @@ import pprint
 import csv
 import pandas as pd
 
-filename = '/Users/arnav/Desktop/YouTube Code/ASSIP-2022/full_data/game.csv'
+filename = '/Users/arnav/Desktop/YouTube Code/ASSIP-2022/full_data/gaming_filtered.csv'
 csvfile = pd.read_csv(filename)
 game_data = csvfile.sample(n=100)
-game_data.to_csv('game_data2.csv')
+game_data.to_csv('game_data1.csv')
