4b146c1474c3870cefb654bf425157dff5cb54de
Arnav <arnav.mach@gmail.com>
2022-07-21 10:00:31 -0500

    sample.py

---
+++ python_scripts/sample.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import argparse
+import json
+import numpy as np
+import pprint
+import csv
+import pandas as pd
+
+csvfile = pd.read_csv('Desktop\YouTube_Code\ASSIP-2022\full_data\game.csv')
+
+csvfile.sample(n = 100)
+
